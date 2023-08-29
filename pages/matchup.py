857df5d41be0555056bf0a1843148d3174a103b7
vyastest <142485679+vyastest@users.noreply.github.com>
2023-08-29 16:31:17 +0200

    Update matchup.py

--- pages/matchup.py
+++ pages/matchup.py
@@ -5,14 +5,14 @@ import pandas
 
 
 #head to head
-newdata=pd.read_csv('teststream.csv')
-headtohead=newdata.groupby(['striker','bowler']).agg({'runs_off_bat': 'sum', 'match_id': 'nunique', 'ball_count': 'sum', 'bowler_out': 'sum','dotball': 'sum','boundary': 'sum'}).reset_index()
+#newdata=pd.read_csv('teststream.csv')
+#headtohead=newdata.groupby(['striker','bowler']).agg({'runs_off_bat': 'sum', 'match_id': 'nunique', 'ball_count': 'sum', 'bowler_out': 'sum','dotball': 'sum','boundary': 'sum'}).reset_index()
 #headtohead[headtohead['striker']=='V Kohli']
 #st.write(headtohead(x)
 
 with st.container():
     st.title("WELCOME TO matchups")
-    st.sidebar.title("Navigation")
-    x = st.sidebar.selectbox("which city data do u want", headtohead['striker'].unique())
-    city_data = headtohead[headtohead['striker'] == x]
-    st.write(city_data)
+    #st.sidebar.title("Navigation")
+    #x = st.sidebar.selectbox("which city data do u want", headtohead['striker'].unique())
+    #city_data = headtohead[headtohead['striker'] == x]
+    #st.write(city_data)

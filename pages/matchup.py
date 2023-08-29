0a8b6ae119944732fff92fe6f7c5375541249975
vyastest <142485679+vyastest@users.noreply.github.com>
2023-08-29 16:10:11 +0200

    Create matchup.py

---
+++ pages/matchup.py
@@ -0,0 +1,18 @@
+
+import streamlit
+import pandas
+
+
+
+#head to head
+newdata=pd.read_csv('teststream.csv')
+headtohead=newdata.groupby(['striker','bowler']).agg({'runs_off_bat': 'sum', 'match_id': 'nunique', 'ball_count': 'sum', 'bowler_out': 'sum','dotball': 'sum','boundary': 'sum'}).reset_index()
+#headtohead[headtohead['striker']=='V Kohli']
+#st.write(headtohead(x)
+
+with st.container():
+    st.title("WELCOME TO matchups")
+    st.sidebar.title("Navigation")
+    x = st.sidebar.selectbox("which city data do u want", headtohead['striker'].unique())
+    city_data = headtohead[headtohead['striker'] == x]
+    st.write(city_data)

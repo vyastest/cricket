de835df26656473a7c15c7e1274d72fe6f380017
vyastest <142485679+vyastest@users.noreply.github.com>
2023-08-29 17:27:59 +0200

    Update matchup.py

--- pages/matchup.py
+++ pages/matchup.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 
 #head to head
-newdata=pd.read_csv('pages/teststream.csv')
+newdata=pd.read_csv('teststream.csv')
 headtohead=newdata.groupby(['striker','bowler']).agg({'runs_off_bat': 'sum', 'match_id': 'nunique', 'ball_count': 'sum', 'bowler_out': 'sum','dotball': 'sum','boundary': 'sum'}).reset_index()
 #headtohead[headtohead['striker']=='V Kohli']
 #st.write(headtohead(x)

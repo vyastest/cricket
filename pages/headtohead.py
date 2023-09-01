e3bbb24d4db3baf1b537ad6c05591efa14d8c0e2
vyastest <142485679+vyastest@users.noreply.github.com>
2023-09-01 11:35:38 +0200

    Update headtohead.py

--- pages/headtohead.py
+++ pages/headtohead.py
@@ -1,1 +1,22 @@
+import streamlit as st
+import pandas as pd
 
+# Create a Streamlit app
+st.title("Filtered DataFrame")
+
+# Sample DataFrame
+df=pd.read_csv('consolidatedresults.csv')
+
+# Define the dropdowns for column 1 and column 2
+selected_column_1 = st.selectbox("Select column 1:", df["team"].unique())
+selected_column_2 = st.selectbox("Select column 2:", df["team_1"].unique())
+
+# Filter the DataFrame
+filtered_df = df[
+    ((df["team"] == selected_column_1) & (df["team_1"] == selected_column_2)) |
+    ((df["team"] == selected_column_2) & (df["team_1"] == selected_column_1))
+]
+
+# Display the filtered DataFrame
+st.write("Filtered DataFrame:")
+st.write(filtered_df)
